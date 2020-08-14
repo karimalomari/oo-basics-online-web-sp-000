974b04226eae2531b7724774dd57001283701964
Karim Alomari <karim.alomari@gmail.com>
2020-08-14 14:33:39 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,9 @@
 class Shoe
   attr_accessor :brand, :color, :size, :material, :condition
   
+  def initialize(brand)
+    @brand = brand
+  end
   
   def cobble
     puts "Your shoe is as good as new!"
